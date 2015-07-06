67450a2383582431bffe47dd18b98c98f5c6418d
Antonio Filho <antonio.filho@dinda.com.br>
2015-07-06 15:03:10 -0300

    Add void method to cancel a sale

--- lib/braspag-rest/request.rb
+++ lib/braspag-rest/request.rb
@@ -2,9 +2,22 @@ module BraspagRest
   class Request
     class << self
       SALE_ENDPOINT = '/v2/sales/'
+      VOID_ENDPOINT = '/void'
 
       def authorize(request_id, params)
         gateway_response = RestClient.post(sale_url, params.to_json, default_headers.merge('RequestId' => request_id))
+        BraspagRest::Response.new(gateway_response)
+      rescue RestClient::ExceptionWithResponse => e
+        config.logger.warn("[BraspagRest] #{e}") if config.log_enabled?
+        BraspagRest::Response.new(e.response)
+      rescue RestClient::Exception => e
+        config.logger.error("[BraspagRest] #{e}") if config.log_enabled?
+        raise
+      end
+
+      def void(request_id, payment_id, amount)
+        params = { Amount: amount }.to_json
+        gateway_response = RestClient.post(void_url(payment_id), params, default_headers.merge('RequestId' => request_id))
         BraspagRest::Response.new(gateway_response)
       rescue RestClient::ExceptionWithResponse => e
         config.logger.warn("[BraspagRest] #{e}") if config.log_enabled?
@@ -18,6 +31,10 @@ module BraspagRest
 
       def sale_url
         config.url + SALE_ENDPOINT
+      end
+
+      def void_url(payment_id)
+        sale_url + payment_id + VOID_ENDPOINT
       end
 
       def default_headers
