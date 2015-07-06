b1457f4028929693967b462a85bc4bf1a18aab86
Antonio Filho <antonio.filho@dinda.com.br>
2015-07-06 15:17:29 -0300

    Implement cancel sale

--- lib/braspag-rest/sale.rb
+++ lib/braspag-rest/sale.rb
@@ -8,6 +8,7 @@ module BraspagRest
     property :order_id, from: 'MerchantOrderId'
     property :customer, from: 'Customer', with: ->(values) { BraspagRest::Customer.new(values) }
     property :payment, from: 'Payment', with: ->(values) { BraspagRest::Payment.new(values) }
+    property :cancelled
 
     coerce_key :customer, BraspagRest::Customer
     coerce_key :payment, BraspagRest::Payment
@@ -24,10 +25,21 @@ module BraspagRest
       true
     end
 
+    def cancel(amount = nil)
+      response = BraspagRest::Request.void(request_id, payment.id, (amount || payment.amount))
+
+      if response.success?
+        initialize_attributes('Payment' => response.parsed_body)
+        self.cancelled = true
+      else
+        initialize_errors(response.parsed_body) and return false
+      end
+    end
+
     private
 
     def initialize_errors(errors)
-      @errors = errors.map{ |error| { code: error['Code'], message: error['Message'] } }
+      @errors = errors.map { |error| { code: error['Code'], message: error['Message'] } }
     end
   end
 end
