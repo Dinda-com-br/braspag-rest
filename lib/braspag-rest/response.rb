e34c6de0bb0facfab3d88db3a2f3738fff5881c8
Antonio Filho <antonio.filho@dinda.com.br>
2015-10-28 07:49:46 -0200

    Fix success method to return false when an error happens

--- lib/braspag-rest/response.rb
+++ lib/braspag-rest/response.rb
@@ -7,7 +7,7 @@ module BraspagRest
     end
 
     def success?
-      (200..207).include?(@gateway_response.code)
+      (200..207).include?(@gateway_response.code) rescue false
     end
 
     def parsed_body
