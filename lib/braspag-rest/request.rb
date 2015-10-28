4d3a1ee254316673a7b9dfa9c6e6b06a96fb2d08
Antonio Filho <antonio.filho@dinda.com.br>
2015-10-28 09:15:54 -0200

    Improve logging

--- lib/braspag-rest/request.rb
+++ lib/braspag-rest/request.rb
@@ -6,24 +6,32 @@ module BraspagRest
       CAPTURE_ENDPOINT = '/capture'
 
       def authorize(request_id, params)
+        config.logger.info("[BraspagRest][Authorize] endpoint: #{sale_url}, params: #{params.to_json}") if config.log_enabled?
+
         execute_braspag_request do
           RestClient.post(sale_url, params.to_json, default_headers.merge('RequestId' => request_id))
         end
       end
 
       def void(request_id, payment_id, amount)
+        config.logger.info("[BraspagRest][Void] endpoint: #{void_url(payment_id, amount)}") if config.log_enabled?
+
         execute_braspag_request do
           RestClient.put(void_url(payment_id, amount), nil, default_headers.merge('RequestId' => request_id))
         end
       end
 
       def get_sale(request_id, payment_id)
+        config.logger.info("[BraspagRest][GetSale] endpoint: #{search_sale_url(payment_id)}") if config.log_enabled?
+
         execute_braspag_request do
           RestClient.get(search_sale_url(payment_id), default_headers.merge('RequestId' => request_id))
         end
       end
 
       def capture(request_id, payment_id, amount)
+        config.logger.info("[BraspagRest][Capture] endpoint: #{capture_url(payment_id)}, amount: #{amount}") if config.log_enabled?
+
         execute_braspag_request do
           RestClient.put(capture_url(payment_id), { Amount: amount }.to_json, default_headers.merge('RequestId' => request_id))
         end
@@ -33,15 +41,18 @@ module BraspagRest
 
       def execute_braspag_request(&block)
         gateway_response = block.call
+
+        config.logger.info("[BraspagRest][Response] gateway_response: #{gateway_response.inspect}") if config.log_enabled?
+
         BraspagRest::Response.new(gateway_response)
       rescue RestClient::ResourceNotFound => e
-        config.logger.error("[BraspagRest] #{e}") if config.log_enabled?
+        config.logger.error("[BraspagRest][Error] message: #{e.message}, status: #{e.http_code}, body: #{e.http_body.inspect}") if config.log_enabled?
         raise
       rescue RestClient::ExceptionWithResponse => e
-        config.logger.warn("[BraspagRest] #{e}") if config.log_enabled?
+        config.logger.warn("[BraspagRest][Error] message: #{e.message}, status: #{e.http_code}, body: #{e.http_body.inspect}") if config.log_enabled?
         BraspagRest::Response.new(e.response)
       rescue RestClient::Exception => e
-        config.logger.error("[BraspagRest] #{e}") if config.log_enabled?
+        config.logger.error("[BraspagRest][Error] message: #{e.message}, status: #{e.http_code}, body: #{e.http_body.inspect}") if config.log_enabled?
         raise
       end
 
