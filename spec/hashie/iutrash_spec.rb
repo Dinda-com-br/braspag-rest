e622205d5de17676482bb3a1977178bac2c43c61
Daniel Urbano <dnl.stunts@gmail.com>
2016-09-21 14:48:11 -0300

    Fixing tests

--- spec/hashie/iutrash_spec.rb
+++ spec/hashie/iutrash_spec.rb
@@ -47,8 +47,8 @@ describe Hashie::IUTrash do
 
     it 'nested inverse attributes' do
       params['Telephones'] = [
-         { 'Code' => '+55', 'Number' => '555-555' },
-         { 'Code' => '+1', 'Number' => '222-222' }
+         { :code => '+55', :number => '555-555' },
+         { :code => '+1', :number => '222-222' }
       ]
       person = Person.new(params)
 
