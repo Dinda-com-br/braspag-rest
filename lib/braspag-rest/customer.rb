e44d241978d4a2abe68456a4753be708d6775631
Carlos Henrique <carlosoteras@gmail.com>
2017-10-01 16:29:47 -0300

    New field, specify type of identity.

--- lib/braspag-rest/customer.rb
+++ lib/braspag-rest/customer.rb
@@ -2,5 +2,6 @@ module BraspagRest
   class Customer < Hashie::IUTrash
     property :name, from: 'Name'
     property :identity, from: 'Identity'
+    property :identity_type, from: 'IdentityType'
   end
 end
--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -4,7 +4,8 @@ describe BraspagRest::Customer do
   let(:braspag_response) {
     {
       'Name' => 'Comprador Teste',
-      'Identity' => '790.010.515-88'
+      'Identity' => '790.010.515-88',
+      'IdentityType' => 'CPF'
     }
   }
 
@@ -14,6 +15,7 @@ describe BraspagRest::Customer do
     it 'initializes a customer using braspag response format' do
       expect(customer.name).to eq('Comprador Teste')
       expect(customer.identity).to eq('790.010.515-88')
+      expect(customer.identity_type).to eq('CPF')
     end
   end
 end
