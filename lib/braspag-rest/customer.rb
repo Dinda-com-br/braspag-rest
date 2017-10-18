bfd491c52fb9673842c9f2da547632a9756b9ba5
Carlos Henrique <carlosoteras@gmail.com>
2017-10-18 09:55:30 -0200

    New fields for registered bank slip (#22)
    * New field, specify type of identity.
    
    * Add new field boleto_number, it specify number of boleto.
    
    * New class responsible for mapping an address to customer.
    
    * Add address association on customer.
    
    * Update version.

--- lib/braspag-rest/customer.rb
+++ lib/braspag-rest/customer.rb
@@ -2,5 +2,7 @@ module BraspagRest
   class Customer < Hashie::IUTrash
     property :name, from: 'Name'
     property :identity, from: 'Identity'
+    property :identity_type, from: 'IdentityType'
+    property :address, from: 'Address', with: ->(values) { BraspagRest::Address.new(values) }
   end
 end
--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -4,7 +4,18 @@ describe BraspagRest::Customer do
   let(:braspag_response) {
     {
       'Name' => 'Comprador Teste',
-      'Identity' => '790.010.515-88'
+      'Identity' => '790.010.515-88',
+      'IdentityType' => 'CPF',
+      'Address' => {
+        'Street' => 'Alameda Xingu',
+        'Number' => '512',
+        'Complement' => '27 andar',
+        'ZipCode' => '12345987',
+        'City' => 'São Paulo',
+        'State' => 'SP',
+        'Country' => 'BRA',
+        'District' => 'Alphaville'
+      }
     }
   }
 
@@ -14,6 +25,8 @@ describe BraspagRest::Customer do
     it 'initializes a customer using braspag response format' do
       expect(customer.name).to eq('Comprador Teste')
       expect(customer.identity).to eq('790.010.515-88')
+      expect(customer.identity_type).to eq('CPF')
+      expect(customer.address).to be_an_instance_of(BraspagRest::Address)
     end
   end
 end
